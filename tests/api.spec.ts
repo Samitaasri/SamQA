76877994bef3653c04cd9939d5421a31ab09a2ec
Samitaasri <sami1ramesh@gmail.com>
2026-03-22 15:47:16 +0530

    APIPush

--- tests/api.spec.ts
+++ tests/api.spec.ts
@@ -7,3 +7,24 @@ test('Get users API',async ({request}) => {
    expect(response.status()).toBe(200);
 
 });
+
+test('Post data using post API',async ({request}) => {
+
+   const response= await request.post('https://jsonplaceholder.typicode.com/users',{
+      data:{
+         name:"Samitaa",
+         username:"Samitaa_QA",
+         email:"samitaa@trichy.com"
+      },
+      headers:{
+         'Content-Type': 'application/json'
+      }
+   })
+   const responseBody= await response.json();
+   console.log(responseBody);
+
+   expect(response.status()).toBe(201);
+   expect(responseBody.name).toBe("Samitaa");
+
+}
+)
